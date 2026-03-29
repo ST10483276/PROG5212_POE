a89d9a83e03314eace7d8fdaa4c617ecc4af7c36
Jason Barasa <st10483276@myemeris.edu.za>
2026-03-29 15:34:27 +0200

    Added registration prompts

---
+++ target/src/main/java/com/mycompany/st10483276poe_26/ST10483276POE_26.java
@@ -0,0 +1,27 @@
+package com.mycompany.st10483276poe_26;
+
+import java.util.Scanner;
+
+public class ST10483276POE_26 {
+
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in); 
+     
+     
+     System.out.println("----------WELCOME TO THE CHAT APP----------");
+     
+     System.out.println("Enter your First Name");
+     
+     
+     System.out.println("Enter your Last Name");
+     
+     
+     System.out.println("Enter in your Cellphone Number");
+     
+     
+     System.out.println("Create a Username");
+     
+     
+     System.out.println("Create a password");
+    }
+}
