c4285aed14a7e9d383671c67648241867d806284
Jason Barasa <st10483276@myemeris.edu.za>
2026-03-29 15:22:04 +0200

    First commit

---
+++ src/main/java/com/mycompany/st10483276poe_26/ST10483276POE_26.java
@@ -0,0 +1,13 @@
+package com.mycompany.st10483276poe_26;
+
+import java.util.Scanner;
+
+public class ST10483276POE_26 {
+
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);  
+     
+     
+     System.out.println("----------WELCOME TO THE CHAT APP----------");
+    }
+}
