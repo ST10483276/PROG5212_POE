b951297a94d987ed8a54526bc7356058d397660a
Jason Barasa <st10483276@myemeris.edu.za>
2026-04-02 10:16:52 +0200

    Add Login class and integrate login flow
    Add registerUser, loginUser, and returnLoginStatus to handle registration and authentication, collect user input, call registerUser, display registration status, and perform a login attempt when registration is successful.

--- src/main/java/com/mycompany/st10483276poe_26/Login.java
+++ src/main/java/com/mycompany/st10483276poe_26/Login.java
@@ -2,10 +2,58 @@ package com.mycompany.st10483276poe_26;
 
 public class Login {
     //Used to store the user data in memory once entered
-    private String FirstName;
-    private String LastName;
-    private String CellNumber;
+    private String firstName;
+    private String lastName;
+    private String cellNumber;
     private String Username;
     private String Password;
     
+    public boolean checkUserName(String Username) {
+        return Username.contains("_") && Username.length()<=5;
+    }
+    
+    public boolean checkPassword(String Password){
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!_\\-*~]).{8,}$";
+        return Password.matches(regex);
+    }
+    
+    public boolean checkCellPhoneNumber(String cellNumber){
+        String regex = "^(\\\\+27 |0)[6-8][0-9]{8}$";
+        return cellNumber.matches(regex);
+    }
+    
+    public String registerUser(String Username, String Password, String firstName, String cellNumber, String cellNumber1){
+        if (!checkUserName(Username)) {
+            return "Incorrect username format! please try again and make sure your username has an underscore '_' and charcaters are not longer than 5.";
+        }
+        if (!checkPassword(Password)) {
+            return "Incorrect password format! please try again and make sure your password has the at least 8 characters, a Captial and small letter, a number and a special character.";
+        }
+        if (!checkCellPhoneNumber(cellNumber)) {
+            return "Incorrect cellphone format! please try again and make sure your have the correct format and your internation code is a South African number starting with +27.";
+        }
+      
+        this.Username = Username;
+        this.Password = Password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cellNumber = cellNumber;
+        
+        return "Registration process complete, The Username, Password, and Cell phone have been captured";     
+    }
+    
+    public boolean loginUser(String enteredUsername, String enteredPassword){
+        if (Username == null || Password == null) {
+            return false;
+        }
+        return enteredUsername.equals(Username) && enteredPassword.equals(Password);
+    }
+    
+    public String returnLoginStatus(boolean loginSuccessful) {
+        if (loginSuccessful) {
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
 }
--- src/main/java/com/mycompany/st10483276poe_26/ST10483276POE_26.java
+++ src/main/java/com/mycompany/st10483276poe_26/ST10483276POE_26.java
@@ -6,22 +6,48 @@ public class ST10483276POE_26 {
 
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in); 
+     Login loginSystem = new Login(); // instantiate for Login.java class
          
      System.out.println("----------WELCOME TO THE CHAT APP----------");
      
      System.out.println("Enter your First Name:"); //Output message for the first name
-     String FirstName = scanner.nextLine(); 
+     String firstName = scanner.nextLine(); 
      
      System.out.println("Enter your Last Name:"); //Output message for the surname 
-     String LastName = scanner.nextLine();
+     String lastName = scanner.nextLine();
      
      System.out.println("Enter in your Cellphone Number (MUST BE A SOUTH AFRICAN NUMBER '+27607740545'):"); //Output message for the cellphone number 
-     String CellNumber = scanner.nextLine();
+     String cellNumber = scanner.nextLine();
      
      System.out.println("Create a Username(<= 5 CHAR & MANDATORY '_'):"); //Output message for the username
      String Username = scanner.nextLine();
      
      System.out.println("Create a password (MIN 8 CHAR, 1 UPPERCARE 1 LOWERCASE & 1 SPECIAL CHAR):"); //Output message for the password
      String Password = scanner.nextLine();
+     
+     String registrationMessage = loginSystem.registerUser(Username, Password, firstName, lastName, cellNumber);
+     
+     System.out.println("\n--- Status ---");
+     System.out.println(registrationMessage);
+     
+     //ONLY WORKS IF LOGIN WAS SUCCESSFUL
+     if (registrationMessage.contains("Successful")){
+        System.out.println("\n=== Login ---");{
+        
+        System.out.println("Enter Username: ");
+        String loginUser = scanner.nextLine();
+        
+        System.out.println("Enter your Password: ");
+        String loginPass = scanner.nextLine();
+        
+        boolean isLogged = loginSystem.loginUser(loginUser, loginPass);
+        String loginStatusMessage = loginSystem.returnLoginStatus(isLogged); 
+        
+        System.out.println("\n--- Login Status ---");
+        System.out.println(loginStatusMessage); 
     }
+        
+     scanner.close();
+    }
+   }
 }
